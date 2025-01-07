9f9513c1e565a6a5c62be8f41c34854fc100fc08
IF270 <incinerationtothesky@gmail.com>
2025-01-07 08:27:32 +0800

    Reading page functionality

--- script.js
+++ script.js
@@ -21,8 +21,9 @@ for (let i = 0; i < headerButtons.length; i++) {
     });
 }
 
-//LOAD JSON DATA//
-function fetchJSONData(filepath, dataHandler) {
+//LOAD FILE DATA//
+const endpoint = "https://infernalfury270.github.io/toranoko/";
+function fetchFileData(filepath, dataHandler) {
     fetch(filepath)
     .then((res) => {
         if (!res.ok) {
@@ -45,7 +46,7 @@ function LoadChangelogs(data) {
     let changelogError = document.querySelector("#changelogError");
     if (data.changelogs.length != 0) {
         changelogError.remove();
-        for (let i = 0; i < data.changelogs.length; i++) {
+        for (let i = data.changelogs.length - 1; i >= 0; i--) {
             let clonedEntry = changelogEntrySample.cloneNode(true);
             clonedEntry.querySelector("p:nth-of-type(1)").innerHTML = data.changelogs[i].date;
             clonedEntry.querySelector("p:nth-of-type(2)").innerHTML = data.changelogs[i].log;
@@ -54,4 +55,25 @@ function LoadChangelogs(data) {
     }
     changelogEntrySample.remove();
 }
-fetchJSONData("https://infernalfury270.github.io/toranoko/data/changelogs.json", LoadChangelogs);
+fetchFileData(endpoint + "data/changelogs.json", LoadChangelogs);
+
+//Chapters//
+var readSeries = document.querySelector("#readSeries");
+function ReadSeries(seriesName) {
+    readSeries.querySelector("h2").innerHTML = seriesName;
+    fetchFileData(endpoint + "data/writingpieces.json", function(data) {
+        readSeries.querySelector("p:nth-of-type(1)").innerHTML = data[seriesName][0].description;
+        readSeries.querySelector("img").src = data[seriesName][0].imageFilepath;
+        let entryContainer = readSeries.querySelector("div:nth-of-type(1)");
+        while (entryContainer.firstChild) {
+            entryContainer.removeChild(myNode.lastChild);
+        }
+        for (let i = 0; i < data[seriesName][0].entries.length; i++) {
+            let newNode = document.createElement("a");
+            newNode.href = data[seriesName][0].entries[i].entryFilepath;
+            newNode.appendChild(document.createTextNode(data[seriesName][0].entries[i].entryName));
+            entryContainer.appendChild(newNode);
+        }
+    })
+}
+ReadSeries("Newsletters");
